f2d9f4d8379340578ebc0a5c360831ac7e8c97ba
Andrea Duina <andreaduina99@gmail.com>
2025-02-04 17:03:56 +0100

    fixed import issues

--- sqil_core/__init__.py
+++ sqil_core/__init__.py
@@ -3,4 +3,9 @@ import sqil_core.resonator as resonator
 
 from .utils import extract_h5_data, read_param_dict
 
-__all__ = ["extract_h5_data", "read_param_dict"]
+# __all__ = ["fit", "extract_h5_data", "read_param_dict"]
+
+# Explicitly remove excluded names from the global namespace
+_exclude_names = ["config"]
+for _name in _exclude_names:
+    globals().pop(_name, None)
