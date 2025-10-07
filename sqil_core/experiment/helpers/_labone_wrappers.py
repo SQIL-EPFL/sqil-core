af3705973263a91e24c1d859337c3cdc3e44fa52
Andrea Duina <andreaduina22@gmail.com>
2025-10-08 00:22:21 +0200

    test

--- sqil_core/experiment/helpers/_labone_wrappers.py
+++ sqil_core/experiment/helpers/_labone_wrappers.py
@@ -6,11 +6,11 @@ from laboneq import serializers
 def w_save(obj, path):
     serializers.save(obj, path)
     # Re-open the file, read the content, and re-save it pretty-printed
-    with open(path, "r", encoding="utf-8") as f:  # ABCD
+    with open(path, "r", encoding="utf-8") as f:  # ABCDE
         data = json.load(f)
     with open(path, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
-x = "maybe I need to add something here1"
-y = x(  )
+with open("test", "r", encoding="utf-8") as f:
+    pass
